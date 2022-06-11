849aad3d43e46532c60bdce7897ab1f9ce89f987
kgagne15 <kristinagagne@ymail.com>
2022-06-11 07:50:52 -0400

    function 13 complete

--- 13_capitalize/capitalize.py
+++ 13_capitalize/capitalize.py
@@ -7,3 +7,11 @@ def capitalize(phrase):
         >>> capitalize('only first word')
         'Only first word'
     """
+    new_phrase = []
+    for letter in phrase:
+        new_phrase.append(letter)
+    new_first = new_phrase[0].upper()
+    new_phrase.pop(0)
+    new_phrase.insert(0, new_first)
+    new_phrase = ''.join(new_phrase)
+    return new_phrase
