a4a8f389d7827f53271e42cbcfaf96146bbaebfb
kgagne15 <kristinagagne@ymail.com>
2022-06-11 14:00:56 -0400

    function 39 complete with help

--- 39_find_greater_numbers/find_greater_numbers.py
+++ 39_find_greater_numbers/find_greater_numbers.py
@@ -19,3 +19,11 @@ def find_greater_numbers(nums):
         >>> find_greater_numbers([])
         0
     """
+    #needed solution 
+    total = 0
+    
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] < nums[j]:
+                total += 1
+    return total
