c8f0fa0201f502459558ebd38ae02fdd6e34ddb6
kgagne15 <kristinagagne@ymail.com>
2022-06-11 10:10:50 -0400

    Function 27 complete

--- 27_titleize/titleize.py
+++ 27_titleize/titleize.py
@@ -7,3 +7,19 @@ def titleize(phrase):
         >>> titleize('oNLy cAPITALIZe fIRSt')
         'Only Capitalize First'
     """
+    total_phrase = []
+    new_word = []
+    split_phrase = phrase.split()
+    for word in split_phrase:
+        for letter in word:
+            new_word.append(letter.lower())
+        new_letter = new_word[0].upper()
+        new_word.pop(0)
+        new_word.insert(0, new_letter)
+        new_word = ''.join(new_word)
+        total_phrase.append(new_word)
+        new_word = []
+    return ' '.join(total_phrase)
+    
+        
+
