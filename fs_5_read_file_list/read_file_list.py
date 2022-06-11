4b486a907320d698aa5150d715a0cc89c0a6b202
kgagne15 <kristinagagne@ymail.com>
2022-06-11 14:55:09 -0400

    function fs_5 complete with help

--- fs_5_read_file_list/read_file_list.py
+++ fs_5_read_file_list/read_file_list.py
@@ -19,3 +19,13 @@ def read_file_list(filename):
     # hint: when you read lines of files, there will be a "newline"
     # (end-of-line character) at the end of each line, and you want to
     # strip that off before you print it. Do some research on that!
+
+
+    file = open(filename, 'r')
+
+    for line in file:
+        line = line.rstrip()
+        print('- ' + line)
+
+#helpful link: https://stackoverflow.com/questions/275018/how-do-i-remove-a-trailing-newline
+#did use solution for hint
