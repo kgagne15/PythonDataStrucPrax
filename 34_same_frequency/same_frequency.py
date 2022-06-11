854232c927a639c04f777c09423bcade1bf89624
kgagne15 <kristinagagne@ymail.com>
2022-06-11 13:37:27 -0400

    function 34 complete

--- 34_same_frequency/same_frequency.py
+++ 34_same_frequency/same_frequency.py
@@ -10,3 +10,23 @@ def same_frequency(num1, num2):
         >>> same_frequency(1212, 2211)
         True
     """
+    loop_dict_one = {}
+    for digit in str(num1):
+        loop_dict_one[digit] = 0
+    for digit in str(num1):
+        loop_dict_one[digit] += 1
+
+
+    loop_dict_two = {}
+    for digit in str(num2):
+        loop_dict_two[digit] = 0
+    for digit in str(num2):
+        loop_dict_two[digit] += 1
+    
+
+    for digit in str(num1):
+        if loop_dict_one[digit] != loop_dict_two[digit]:
+            return False
+    return True
+
+    #helpful link: https://www.knowprogram.com/python/python-iterate-over-digits-in-integer/
