7bdd93c9c50bfbc939e0c015eeab2fb532863ec4
kgagne15 <kristinagagne@ymail.com>
2022-06-11 14:46:16 -0400

    function fs2 with solution

--- fs_2_valid_parentheses/valid_parentheses.py
+++ fs_2_valid_parentheses/valid_parentheses.py
@@ -22,3 +22,17 @@ def valid_parentheses(parens):
         >>> valid_parentheses(")()(")
         False
     """
+
+    #used solution
+    count = 0
+
+    for p in parens:
+        if p == '(':
+            count += 1
+        elif p == ')':
+            count -= 1
+        
+        if count < 0:
+            return False
+        
+    return count == 0
