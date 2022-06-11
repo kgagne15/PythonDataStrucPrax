1c921c5e72d4dc99885c252c6ec6dd79b0d8df2d
kgagne15 <kristinagagne@ymail.com>
2022-06-11 08:04:57 -0400

    function 14 complete

--- 14_compact/compact.py
+++ 14_compact/compact.py
@@ -4,3 +4,9 @@ def compact(lst):
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
+    new_lst = []
+    for item in lst:
+        new_lst.append(item)
+    
+    true_list = [item for item in new_lst if bool(item) == True]
+    return true_list
