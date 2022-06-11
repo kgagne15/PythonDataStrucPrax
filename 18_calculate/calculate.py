ff8fd64f750e552b81d7a309ab86f25d6a3d573c
kgagne15 <kristinagagne@ymail.com>
2022-06-11 08:44:20 -0400

    function 18 complete

--- 18_calculate/calculate.py
+++ 18_calculate/calculate.py
@@ -26,3 +26,37 @@ def calculate(operation, a, b, make_int=False, message='The result is'):
         >>> calculate('foo', 2, 3)
         
     """
+    if operation == 'add':
+        if make_int == False:
+            sum = a + b
+            return message + ' ' + str(sum)
+        else:
+            sum = a + b
+            sum = int(sum)
+            return message + ' ' + str(sum)
+    elif operation == 'subtract':
+        if make_int == False:
+            diff = a - b
+            return message + ' ' + str(diff)
+        else:
+            diff = a - b
+            diff = int(diff)
+            return message + ' ' + str(diff)
+    elif operation == 'multiply':
+        if make_int == False:
+            prod = a * b
+            return message + ' ' + str(prod)
+        else:
+            prod = a * be
+            prod = int(prod)
+            return message + ' ' + str(prod)
+    elif operation == 'divide':
+        if make_int == False:
+            res = a / b
+            return message + ' ' + str(res)
+        else:
+            res = a / b
+            res = int(res)
+            return message + ' ' + str(res)
+    else:
+        return None
