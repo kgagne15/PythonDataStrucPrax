be1e417a3d91159062b31b1d3dbe440b2915c39d
kgagne15 <kristinagagne@ymail.com>
2022-06-11 09:44:56 -0400

    function 26 complete

--- 26_vowel_count/vowel_count.py
+++ 26_vowel_count/vowel_count.py
@@ -7,3 +7,17 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    loop_dict = {}
+    vowels = 'aeiou'
+    for letter in phrase:
+        new_letter = letter.lower()
+        for vowel in vowels:
+            if new_letter == vowel:
+                loop_dict[new_letter] = 0
+        
+    for letter in phrase:
+        new_letter = letter.lower()
+        for vowel in vowels:
+            if new_letter == vowel:
+                loop_dict[new_letter] += 1
+    return loop_dict
